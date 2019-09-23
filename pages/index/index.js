2c0c25ad4e2f6a3ca2bb7c2b10eec0d2c1e2b4c8
wuming <928118279@qq.com>
2019-09-23 20:44:16 +0800

    首页——导航

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,11 +5,14 @@
 Page({
   data: {
     // 轮播图数组
-    swiperList: []
+    swiperList: [],
+    // 导航数组
+    catitemList: []
   },
 
   onLoad() {
     this.getSwiperData()
+    this.getCatitems()
   },
 
   // 获取轮播图数据
@@ -20,6 +23,19 @@ Page({
         // console.table(result.data.message)
         this.setData({
           swiperList: result.data.message
+        })
+      }
+    })
+  },
+
+  // 获取导航数据
+  getCatitems() {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/catitems',
+      success: (result) => {
+        // console.table(result.data.message)
+        this.setData({
+          catitemList: result.data.message
         })
       }
     })
