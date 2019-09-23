8401bb0feb84a78f65cbda00712a302ea5fa46ae
wuming <928118279@qq.com>
2019-09-23 20:57:34 +0800

    首页之楼层标签的渲染与样式优化

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,12 +7,15 @@ Page({
     // 轮播图数组
     swiperList: [],
     // 导航数组
-    catitemList: []
+    catitemList: [],
+    // 楼层数组
+    floorList: []
   },
 
   onLoad() {
     this.getSwiperData()
     this.getCatitems()
+    this.getfloorList()
   },
 
   // 获取轮播图数据
@@ -36,6 +39,19 @@ Page({
         // console.table(result.data.message)
         this.setData({
           catitemList: result.data.message
+        })
+      }
+    })
+  },
+
+  // 获取楼层数据
+  getfloorList() {
+    wx.request({
+      url: 'https://api.zbztb.cn/api/public/v1/home/floordata',
+      success: (result) => {
+        // console.table(result.data.message)
+        this.setData({
+          floorList: result.data.message
         })
       }
     })
